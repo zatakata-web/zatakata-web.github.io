4dacc5e989a2520263e41ca9289c4b22540e61eb
zatakata-web <zatakata98@gmail.com>
2020-06-17 16:07:35 +0300

    added lightbox and fix mobile nav

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
-
 const navLinks = $('.header__nav-link');
 
+const navOpenBtn = $('.header__burger'),
+    navContainer = $('.header__nav'),
+    navCloseBtn = $('.header__nav-close');
+
+
+// scroll
 navLinks.click(e => {
     navLinks.removeClass('active');
     $(e.target).addClass('active');
+    navContainer.removeClass('active')
 })
 
 $('a[href^="#"]').click(function () {
@@ -11,3 +17,13 @@ $('a[href^="#"]').click(function () {
     $('html, body').animate({ scrollTop: $(target).offset().top }, 800);
     return false;
 });
+
+
+// mobile menu
+navOpenBtn.click(function () {
+    navContainer.addClass('active')
+})
+
+navCloseBtn.click(function () {
+    navContainer.removeClass('active')
+})
